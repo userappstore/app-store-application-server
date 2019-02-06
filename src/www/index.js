fe3a9fe01c7607bad3b42253dc282c0d060e0965
Ben Lowry <benlowry@outlook.com>
2019-02-06 08:46:37 -0600

    Added information to home page

---
+++ src/www/index.js
@@ -0,0 +1,54 @@
+const dashboardServer = require('../dashboard-server.js')
+const userAppStore = require('../../index.js')
+let sitemap
+
+module.exports = {
+  get: renderPage
+}
+
+async function renderPage (req, res) {
+  const doc = userAppStore.HTML.parse(req.route.html)
+  const sitemap = await dashboardServer.get(`/api/application-server/sitemap`, null, null)
+  for (const url in global.sitemap) {
+    sitemap[url] = 'application-server'
+  }
+  if (!sitemap) {
+    sitemap = {}
+    const object = 'data'
+    for (const url in sitemap) {
+      const origin = sitemap[url]
+      if (sitemap[origin]) {
+        continue
+      }
+      sitemap[origin] = {
+        web: [],
+        api: []
+      }
+    }
+    for (const url in sitemap) {
+      const origin = sitemap[url]
+      if (url.startsWith('/api/')) {
+        sitemap[origin].api.push({ url })
+      } else {
+        if (url === '/') {
+          sitemap[origin].web.unshift({ url, object })
+        } else {
+          sitemap[origin].web.push({ url, object })  
+        }
+      }
+    }
+  }
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/dashboard'].web, 'url-data', 'dashboard-pages')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/dashboard'].api, 'url-data', 'dashboard-api')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/organizations'].web, 'url-data', 'organizations-module-pages')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/organizations'].api, 'url-data', 'organizations-module-api')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/stripe-subscriptions'].web, 'url-data', 'subscriptions-module-pages')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/stripe-subscriptions'].api, 'url-data', 'subscriptions-module-api')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/stripe-connect'].web, 'url-data', 'connect-module-pages')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/stripe-connect'].api, 'url-data', 'connect-module-api')
+  // userAppStore.HTML.renderList(doc, sitemap['dashboard-server'].web, 'url-data', 'dashboard-server-pages')
+  userAppStore.HTML.renderList(doc, sitemap['dashboard-server'].api, 'url-data', 'dashboard-server-api')
+  userAppStore.HTML.renderList(doc, sitemap['application-server'].web, 'url-data', 'application-server-pages')
+  userAppStore.HTML.renderList(doc, sitemap['application-server'].api, 'url-data', 'application-server-api')
+  return res.end(doc.toString())
+}
