73647ca9b9890fb77304a96e0d863de8819d5e25
Ben Lowry <benlowry@outlook.com>
2019-02-19 13:11:10 -0600

    No @s

--- src/www/index.js
+++ src/www/index.js
@@ -35,7 +35,6 @@ async function renderPage (req, res) {
         apiUser: []
       }
       const repo = route.origin.substring(1)
-      let githubNodeJSURL, githubHTMLURL, githubTestURL
       if (route.nodejs) {
         route.githubNodeJSURL = `https://github.com/${repo}/blob/master/src/www${url}.js`
       }
@@ -68,26 +67,26 @@ async function renderPage (req, res) {
       }
     }
   }
-  sitemap['@userappstore/dashboard'].web = sitemap['@userappstore/dashboard'].webUser.concat(sitemap['@userappstore/dashboard'].webAdministrator)
-  sitemap['@userappstore/dashboard'].api = sitemap['@userappstore/dashboard'].apiUser.concat(sitemap['@userappstore/dashboard'].apiAdministrator)
-  sitemap['@userappstore/organizations'].web = sitemap['@userappstore/organizations'].webUser.concat(sitemap['@userappstore/organizations'].webAdministrator)
-  sitemap['@userappstore/organizations'].api = sitemap['@userappstore/organizations'].apiUser.concat(sitemap['@userappstore/organizations'].apiAdministrator)
-  sitemap['@userappstore/stripe-subscriptions'].web = sitemap['@userappstore/stripe-subscriptions'].webUser.concat(sitemap['@userappstore/stripe-subscriptions'].webAdministrator)
-  sitemap['@userappstore/stripe-subscriptions'].api = sitemap['@userappstore/stripe-subscriptions'].apiUser.concat(sitemap['@userappstore/stripe-subscriptions'].apiAdministrator)
-  sitemap['@userappstore/stripe-connect'].web = sitemap['@userappstore/stripe-connect'].webUser.concat(sitemap['@userappstore/stripe-connect'].webAdministrator)
-  sitemap['@userappstore/stripe-connect'].api = sitemap['@userappstore/stripe-connect'].apiUser.concat(sitemap['@userappstore/stripe-connect'].apiAdministrator)
+  sitemap['userappstore/dashboard'].web = sitemap['userappstore/dashboard'].webUser.concat(sitemap['userappstore/dashboard'].webAdministrator)
+  sitemap['userappstore/dashboard'].api = sitemap['userappstore/dashboard'].apiUser.concat(sitemap['userappstore/dashboard'].apiAdministrator)
+  sitemap['userappstore/organizations'].web = sitemap['userappstore/organizations'].webUser.concat(sitemap['userappstore/organizations'].webAdministrator)
+  sitemap['userappstore/organizations'].api = sitemap['userappstore/organizations'].apiUser.concat(sitemap['userappstore/organizations'].apiAdministrator)
+  sitemap['userappstore/stripe-subscriptions'].web = sitemap['userappstore/stripe-subscriptions'].webUser.concat(sitemap['userappstore/stripe-subscriptions'].webAdministrator)
+  sitemap['userappstore/stripe-subscriptions'].api = sitemap['userappstore/stripe-subscriptions'].apiUser.concat(sitemap['userappstore/stripe-subscriptions'].apiAdministrator)
+  sitemap['userappstore/stripe-connect'].web = sitemap['userappstore/stripe-connect'].webUser.concat(sitemap['userappstore/stripe-connect'].webAdministrator)
+  sitemap['userappstore/stripe-connect'].api = sitemap['userappstore/stripe-connect'].apiUser.concat(sitemap['userappstore/stripe-connect'].apiAdministrator)
   // sitemap['application-server'].web = sitemap['application-server'].webUser.concat(sitemap['application-server'].webAdministrator)
   // sitemap['application-server'].api = sitemap['application-server'].apiUser.concat(sitemap['application-server'].apiAdministrator)
   // sitemap['dashboard-server'].web = sitemap['dashboard-server'].webUser.concat(sitemap['dashboard-server'].webAdministrator)
   // sitemap['dashboard-server'].api = sitemap['dashboard-server'].apiUser.concat(sitemap['dashboard-server'].apiAdministrator)
-  userAppStore.HTML.renderList(doc, sitemap['@userappstore/dashboard'].web, 'page-data', 'dashboard-pages')
-  userAppStore.HTML.renderList(doc, sitemap['@userappstore/dashboard'].api, 'api-data', 'dashboard-api')
-  userAppStore.HTML.renderList(doc, sitemap['@userappstore/organizations'].web, 'page-data', 'organizations-module-pages')
-  userAppStore.HTML.renderList(doc, sitemap['@userappstore/organizations'].api, 'api-data', 'organizations-module-api')
-  userAppStore.HTML.renderList(doc, sitemap['@userappstore/stripe-subscriptions'].web, 'page-data', 'subscriptions-module-pages')
-  userAppStore.HTML.renderList(doc, sitemap['@userappstore/stripe-subscriptions'].api, 'api-data', 'subscriptions-module-api')
-  userAppStore.HTML.renderList(doc, sitemap['@userappstore/stripe-connect'].web, 'page-data', 'connect-module-pages')
-  userAppStore.HTML.renderList(doc, sitemap['@userappstore/stripe-connect'].api, 'api-data', 'connect-module-api')
+  userAppStore.HTML.renderList(doc, sitemap['userappstore/dashboard'].web, 'page-data', 'dashboard-pages')
+  userAppStore.HTML.renderList(doc, sitemap['userappstore/dashboard'].api, 'api-data', 'dashboard-api')
+  userAppStore.HTML.renderList(doc, sitemap['userappstore/organizations'].web, 'page-data', 'organizations-module-pages')
+  userAppStore.HTML.renderList(doc, sitemap['userappstore/organizations'].api, 'api-data', 'organizations-module-api')
+  userAppStore.HTML.renderList(doc, sitemap['userappstore/stripe-subscriptions'].web, 'page-data', 'subscriptions-module-pages')
+  userAppStore.HTML.renderList(doc, sitemap['userappstore/stripe-subscriptions'].api, 'api-data', 'subscriptions-module-api')
+  userAppStore.HTML.renderList(doc, sitemap['userappstore/stripe-connect'].web, 'page-data', 'connect-module-pages')
+  userAppStore.HTML.renderList(doc, sitemap['userappstore/stripe-connect'].api, 'api-data', 'connect-module-api')
   // userAppStore.HTML.renderList(doc, sitemap['dashboard-server'].web, 'url-data', 'dashboard-server-pages')
   // userAppStore.HTML.renderList(doc, sitemap['dashboard-server'].api, 'url-data', 'dashboard-server-api')
   // userAppStore.HTML.renderList(doc, sitemap['application-server'].web, 'url-data', 'application-server-pages')
