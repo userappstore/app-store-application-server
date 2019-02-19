8419fe3cd5faea1ca5e2e747a41a6090232648d5
Ben Lowry <benlowry@outlook.com>
2019-02-19 13:53:01 -0600

    Cleaned up home page

--- src/www/index.js
+++ src/www/index.js
@@ -28,13 +28,14 @@ async function renderPage (req, res) {
       route.id = i++
       route.object = 'route'
       route.url = url
-      sitemap[route.origin] = sitemap[route.origin] || {
+      const key = route.origin.startsWith('@') ? route.origin : `@${route.origin}`
+      sitemap[key] = sitemap[key] || {
         webAdministrator: [],
         webUser: [],
         apiAdministrator: [],
         apiUser: []
       }
-      const repo = route.origin.substring(1)
+      const repo = key.substring(1)
       if (route.nodejs) {
         route.githubNodeJSURL = `https://github.com/${repo}/blob/master/src/www${url}.js`
       }
@@ -46,66 +47,74 @@ async function renderPage (req, res) {
       }
       if (url.startsWith('/api/')) {
         if (url.startsWith('/api/user/')) {
-          sitemap[route.origin].apiUser.push(route)
+          sitemap[key].apiUser.push(route)
         } else {
-          sitemap[route.origin].apiAdministrator.push(route)
+          sitemap[key].apiAdministrator.push(route)
         }
         continue
       }
       if (roots.indexOf(url) > 1) {
         if (url.startsWith('/administrator')) {
-          sitemap[route.origin].webAdministrator.unshift(route)
+          sitemap[key].webAdministrator.unshift(route)
         } else {
-          sitemap[route.origin].webUser.unshift(route)
+          sitemap[key].webUser.unshift(route)
         }
       } else { 
         if (url.startsWith('/administrator')) {
-          sitemap[route.origin].webAdministrator.push(route)  
+          sitemap[key].webAdministrator.push(route)  
         } else {
-          sitemap[route.origin].webUser.push(route)
+          sitemap[key].webUser.push(route)
         }
       }
     }
   }
-  sitemap['userappstore/dashboard'].web = sitemap['userappstore/dashboard'].webUser.concat(sitemap['userappstore/dashboard'].webAdministrator)
-  sitemap['userappstore/dashboard'].api = sitemap['userappstore/dashboard'].apiUser.concat(sitemap['userappstore/dashboard'].apiAdministrator)
-  sitemap['userappstore/organizations'].web = sitemap['userappstore/organizations'].webUser.concat(sitemap['userappstore/organizations'].webAdministrator)
-  sitemap['userappstore/organizations'].api = sitemap['userappstore/organizations'].apiUser.concat(sitemap['userappstore/organizations'].apiAdministrator)
-  sitemap['userappstore/stripe-subscriptions'].web = sitemap['userappstore/stripe-subscriptions'].webUser.concat(sitemap['userappstore/stripe-subscriptions'].webAdministrator)
-  sitemap['userappstore/stripe-subscriptions'].api = sitemap['userappstore/stripe-subscriptions'].apiUser.concat(sitemap['userappstore/stripe-subscriptions'].apiAdministrator)
-  sitemap['userappstore/stripe-connect'].web = sitemap['userappstore/stripe-connect'].webUser.concat(sitemap['userappstore/stripe-connect'].webAdministrator)
-  sitemap['userappstore/stripe-connect'].api = sitemap['userappstore/stripe-connect'].apiUser.concat(sitemap['userappstore/stripe-connect'].apiAdministrator)
+  console.log('sitemap', require('util').inspect(sitemap))
+  sitemap['@userappstore/dashboard'].web = sitemap['@userappstore/dashboard'].webUser.concat(sitemap['@userappstore/dashboard'].webAdministrator)
+  sitemap['@userappstore/dashboard'].api = sitemap['@userappstore/dashboard'].apiUser.concat(sitemap['@userappstore/dashboard'].apiAdministrator)
+  sitemap['@userappstore/organizations'].web = sitemap['@userappstore/organizations'].webUser.concat(sitemap['@userappstore/organizations'].webAdministrator)
+  sitemap['@userappstore/organizations'].api = sitemap['@userappstore/organizations'].apiUser.concat(sitemap['@userappstore/organizations'].apiAdministrator)
+  sitemap['@userappstore/stripe-subscriptions'].web = sitemap['@userappstore/stripe-subscriptions'].webUser.concat(sitemap['@userappstore/stripe-subscriptions'].webAdministrator)
+  sitemap['@userappstore/stripe-subscriptions'].api = sitemap['@userappstore/stripe-subscriptions'].apiUser.concat(sitemap['@userappstore/stripe-subscriptions'].apiAdministrator)
+  sitemap['@userappstore/stripe-connect'].web = sitemap['@userappstore/stripe-connect'].webUser.concat(sitemap['@userappstore/stripe-connect'].webAdministrator)
+  sitemap['@userappstore/stripe-connect'].api = sitemap['@userappstore/stripe-connect'].apiUser.concat(sitemap['@userappstore/stripe-connect'].apiAdministrator)
   // sitemap['application-server'].web = sitemap['application-server'].webUser.concat(sitemap['application-server'].webAdministrator)
   // sitemap['application-server'].api = sitemap['application-server'].apiUser.concat(sitemap['application-server'].apiAdministrator)
   // sitemap['dashboard-server'].web = sitemap['dashboard-server'].webUser.concat(sitemap['dashboard-server'].webAdministrator)
   // sitemap['dashboard-server'].api = sitemap['dashboard-server'].apiUser.concat(sitemap['dashboard-server'].apiAdministrator)
-  userAppStore.HTML.renderList(doc, sitemap['userappstore/dashboard'].web, 'page-data', 'dashboard-pages')
-  userAppStore.HTML.renderList(doc, sitemap['userappstore/dashboard'].api, 'api-data', 'dashboard-api')
-  userAppStore.HTML.renderList(doc, sitemap['userappstore/organizations'].web, 'page-data', 'organizations-module-pages')
-  userAppStore.HTML.renderList(doc, sitemap['userappstore/organizations'].api, 'api-data', 'organizations-module-api')
-  userAppStore.HTML.renderList(doc, sitemap['userappstore/stripe-subscriptions'].web, 'page-data', 'subscriptions-module-pages')
-  userAppStore.HTML.renderList(doc, sitemap['userappstore/stripe-subscriptions'].api, 'api-data', 'subscriptions-module-api')
-  userAppStore.HTML.renderList(doc, sitemap['userappstore/stripe-connect'].web, 'page-data', 'connect-module-pages')
-  userAppStore.HTML.renderList(doc, sitemap['userappstore/stripe-connect'].api, 'api-data', 'connect-module-api')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/dashboard'].web, 'page-data', 'dashboard-pages')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/dashboard'].api, 'api-data', 'dashboard-api')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/organizations'].web, 'page-data', 'organizations-module-pages')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/organizations'].api, 'api-data', 'organizations-module-api')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/stripe-subscriptions'].web, 'page-data', 'subscriptions-module-pages')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/stripe-subscriptions'].api, 'api-data', 'subscriptions-module-api')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/stripe-connect'].web, 'page-data', 'connect-module-pages')
+  userAppStore.HTML.renderList(doc, sitemap['@userappstore/stripe-connect'].api, 'api-data', 'connect-module-api')
   // userAppStore.HTML.renderList(doc, sitemap['dashboard-server'].web, 'url-data', 'dashboard-server-pages')
   // userAppStore.HTML.renderList(doc, sitemap['dashboard-server'].api, 'url-data', 'dashboard-server-api')
   // userAppStore.HTML.renderList(doc, sitemap['application-server'].web, 'url-data', 'application-server-pages')
   // userAppStore.HTML.renderList(doc, sitemap['application-server'].api, 'url-data', 'application-server-api')
   const removeLinks = []
-  for (const origin in sitemap) {
-    const route = sitemap[origin]
-    if (!route.html) {
-      removeLinks.push(`html-${route.id}`)
-    }
-    if (!route.js) {
-      removeLinks.push(`nodejs-${route.id}`)
-    }
-    if (!route.test) {
-      removeLinks.push(`test-${route.id}`)
+  for (const key in sitemap) {
+    for (const arrayKey in sitemap[key]){
+      const array = sitemap[key][arrayKey]
+      for (const route of array) {
+        if (!route.githubHTMLURL) {
+          removeLinks.push(`html-${route.id}`)
+        }
+        if (!route.githubNodeJSURL) {
+          removeLinks.push(`nodejs-${route.id}`)
+        }
+        if (!route.githubTestURL) {
+          removeLinks.push(`test-${route.id}`)
+        }
+      }
     }
   }
   for (const id of removeLinks) {
     const link = doc.getElementById(id)
+    if (!link) {
+      continue
+    }
     link.parentNode.removeChild(link)
   }
   return res.end(doc.toString())
