b89455aa07b685e100572c31201e0c098ab12f7a
Ben Lowry <benlowry@outlook.com>
2019-06-13 12:01:35 -0600

    Detect projects via published apps

--- src/www/api/user/userappstore/installed-project-files.js
+++ src/www/api/user/userappstore/installed-project-files.js
@@ -9,11 +9,30 @@ module.exports = {
     const installs = await global.api.user.userappstore.Installs.get(req)
     let found
     for (const install of installs) {
-      if (install.projectid !== req.query.projectid) {
+      if (install.projectid && install.projectid !== req.query.projectid) {
         continue
       }
-      found = true
-      break
+      if (install.projectid === req.query.projectid) {
+        found = true
+        break
+      }
+      if (install.appid) {
+        req.query.appid = install.appid
+        const app = await global.api.user.userappstore.InstalledApp.get(req)
+        if (!app.projectid || app.projectid !== req.query.projectid) {
+          continue
+        }
+        found = true
+        break
+      } else if (install.serverid) {
+        req.query.serverid = install.serverid
+        const server = await global.api.user.userappstore.ApplicationServer.get(req)
+        if (!server.projectid || server.projectid !== req.query.projectid) {
+          continue
+        }
+        found = true
+        break
+      }
     }
     if (!found) {
       throw new Error('invalid-projectid')
